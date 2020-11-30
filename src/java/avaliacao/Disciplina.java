a5b7d9b548b8a8766bed87b55fc77137966cfc03
victor212luan <streamyodin@gmail.com>
2020-11-30 13:53:51 -0300

    Alterações na classe Disciplina, insert, delete e update

--- src/java/avaliacao/Disciplina.java
+++ src/java/avaliacao/Disciplina.java
@@ -5,6 +5,11 @@
  */
 package avaliacao;
 
+import db.DBListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -12,15 +17,26 @@ import java.util.ArrayList;
  * @author Victor
  */
 public class Disciplina {
+    private int id;
     private String nome;
     private String ementa;
     private int ciclo;
-    private double nota;
+    private int nota;
 
-    public Disciplina(String nome, String ementa, int ciclo) {
+    public Disciplina(int id, String nome, String ementa, int ciclo, int nota) {
+        this.id = id;
         this.nome = nome;
         this.ementa = ementa;
         this.ciclo = ciclo;
+        this.nota = nota;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -47,31 +63,136 @@ public class Disciplina {
         this.ciclo = ciclo;
     }
 
-    public double getNota() {
+    public int getNota() {
         return nota;
     }
 
-    public void setNota(double nota) {
+    public void setNota(int nota) {
         this.nota = nota;
     }
     
-    public static ArrayList<Disciplina> getList(){
-        
+    public static ArrayList<Disciplina> getList() throws Exception{
         ArrayList<Disciplina> minhasDisciplinas = new ArrayList<>();
         
-        minhasDisciplinas.add(new Disciplina("Banco de Dados","Conceitos de Base de Dados. Modelos conceituais de informações.",4));
-        minhasDisciplinas.add(new Disciplina("Engenharia de Software III","Conceitos, evolução e importância de arquitetura de software.",4));
-        minhasDisciplinas.add(new Disciplina("Programação Orientada a Objetos","Conceitos e evolução da tecnologia de orientação a objetos.",4));
-        minhasDisciplinas.add(new Disciplina("Linguagem de Programação IV","Comandos de linguagens usadas na construção e estruturação de sites.",4));
-        minhasDisciplinas.add(new Disciplina("Sistemas Operacionais II","Apresentação de um sistema operacional específico utilizado em ambiente corporativo.",4));
-        minhasDisciplinas.add(new Disciplina("Inglês IV","Consolidação da compreensão e produção oral e escrita por meio da integração.",4));
-        minhasDisciplinas.add(new Disciplina("Metodologia da Pesquisa Científico-Tecnológica","O Papel da ciência e da tecnologia. Tipos de conhecimento. Método e técnica.",4));
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet result = null;
         
+        Exception methodException = null;
+        
+        try{
+            conn = DBListener.getConnection();
+            stmt = conn.createStatement();
+            result = stmt.executeQuery("SELECT * FROM disciplinas");
+            
+            while(result.next()){
+                minhasDisciplinas.add(new Disciplina(
+                        result.getInt("id"),
+                        result.getString("nome"),
+                        result.getString("ementa"),
+                        result.getInt("ciclo"),
+                        result.getInt("nota"))
+                );
+            }
+        } catch(Exception ex){
+            methodException = ex;
+        } finally{
+            try{ result.close();}catch(Exception ex1){}
+            try{ stmt.close();}catch(Exception ex1){}
+            try{ conn.close();}catch(Exception ex1){}
+        }
+        
+        if( methodException != null ) throw methodException;
+         
         return minhasDisciplinas;
     }
     
+    public static void insert(String nome, String ementa, int ciclo) throws Exception{
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        
+        Exception methodException = null;
+        
+        try{
+            conn = DBListener.getConnection();
+            stmt = conn.prepareStatement("INSERT INTO disciplinas(nome, ementa, ciclo) values(?,?,?)");
+            
+            stmt.setString(1, nome);
+            stmt.setString(2, ementa);
+            stmt.setInt(3, ciclo);
+            
+            stmt.execute();
+        } catch(Exception ex){
+            methodException = ex;
+        } finally{
+            try{ stmt.close();}catch(Exception ex1){}
+            try{ conn.close();}catch(Exception ex1){}
+        }
+        
+        if( methodException != null ) throw methodException;
+        
+    }
+    
+    public static void update(int nota, int id) throws Exception{
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        
+        Exception methodException = null;
+        
+        try{
+            conn = DBListener.getConnection();
+            stmt = conn.prepareStatement("UPDATE disciplinas SET nota=? WHERE id=?");
+            
+            stmt.setInt(1, nota);
+            stmt.setInt(2, id);
+            
+            stmt.execute();
+        } catch(Exception ex){
+            methodException = ex;
+        } finally{
+            try{ stmt.close();}catch(Exception ex1){}
+            try{ conn.close();}catch(Exception ex1){}
+        }
+        
+        if( methodException != null ) throw methodException;
+        
+    }
+    
+    public static void delete(int id) throws Exception{
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        
+        Exception methodException = null;
+        
+        try{
+            conn = DBListener.getConnection();
+            stmt = conn.prepareStatement("DELETE FROM disciplinas WHERE id=?");
+            
+            stmt.setInt(1, id);
+            
+            stmt.execute();
+        } catch(Exception ex){
+            methodException = ex;
+        } finally{
+            try{ stmt.close();}catch(Exception ex1){}
+            try{ conn.close();}catch(Exception ex1){}
+        }
+        
+        if( methodException != null ) throw methodException;
+        
+    }
+    
     public static String getCreateStatement(){
-        return "";
+        return "CREATE TABLE IF NOT EXISTS disciplinas("
+                + "id INTEGER PRIMARY KEY,"
+                + "nome VARCHAR(50) NOT NULL,"
+                + "ementa VARCHAR(200) NOT NULL,"
+                + "ciclo INTEGER NOT NULL,"
+                + "nota INTEGER DEFAULT 0"
+            + ")";
     }
 }
 
